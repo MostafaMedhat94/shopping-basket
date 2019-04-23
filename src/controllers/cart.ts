c15fbf2f8faf7f8a6e912dd1ee5049d07f588bbc
Mostafa Medhat <mostafa.medhat994@gmail.com>
2019-04-23 06:58:16 +0200

    Add Only ProductsList Available Products

--- src/controllers/cart.ts
+++ src/controllers/cart.ts
@@ -5,35 +5,38 @@ import Product from "../models/Product.model";
 
 const cart = new Cart();
 
+export const getAll = (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send(cart.products);
+};
+
 export const create = (req: Request, res: Response, next: NextFunction) => {
     try {
-        const {
-            product_id,
-            product_name,
-            product_price,
-            product_quantity,
-        } = req.body;
+        const { product_id, product_quantity } = req.body;
 
-        Product.findOne({ product_id }, (err, product) => {
+        // Check for product existence in the productsList
+        Product.findOne({ product_id }, (err, storedProduct) => {
             if (err) {
                 return next(err);
             }
 
-            if (!product) {
+            // If the product is not the productsList
+            if (!storedProduct) {
                 return res.status(400).send({
                     message:
                         "You can only add products available in the Products List!",
                 });
             }
 
-            const cartProducts = cart.addProduct({
-                product_id,
-                product_name,
-                product_price,
-                product_quantity,
-            });
+            // Otherwise, get the product
+            const product = storedProduct.toObject();
 
-            return res.status(201).send(cartProducts);
+            // Update its quantity
+            product.product_quantity = product_quantity;
+
+            // Add the product to the user's shopping cart
+            cart.addProduct(product);
+
+            return res.status(201).send(product);
         });
     } catch (error) {
         return next(error);
